713c3580b1f0a69e56164173a5f6e1a4b7913097
CharlyWelch <blwbiology@gmail.com>
2018-02-06 17:02:43 -0800

    continue app setup/pyramid practice

---
+++ expense_tracker/expense_tracker/views/__init__.py
@@ -0,0 +1,5 @@
+from .default import home_page
+
+def includeme(config):
+    """ List of views to include for the configurator object """
+    config.add_view(home_page, route_name='home')
--- expense_tracker/expense_tracker/views/default.py
+++ expense_tracker/expense_tracker/views/default.py
@@ -1,33 +1,7 @@
-from pyramid.response import Response
-from pyramid.view import view_config
+""" Views for the Expense Tracker app """
 
-from sqlalchemy.exc import DBAPIError
+from pyramid.response import response
 
-from ..models import MyModel
-
-
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
-def my_view(request):
-    try:
-        query = request.dbsession.query(MyModel)
-        one = query.filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'one': one, 'project': 'Expense Tracker'}
-
-
-db_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
-
-1.  You may need to run the "initialize_expense_tracker_db" script
-    to initialize your database tables.  Check your virtual
-    environment's "bin" directory for this script and try to run it.
-
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
-
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
+def home_page(request):
+    """ View for the home route """
+    return Response("This is my first view!")
